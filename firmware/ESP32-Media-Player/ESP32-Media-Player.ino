4720fd1587904acb3bcbbdb1e40b6bb6416bf633
KennethLe9 <kennethle9@gmail.com>
2025-09-01 13:31:18 -0500

    Added the Button constructor

--- firmware/ESP32-Media-Player/ESP32-Media-Player.ino
+++ firmware/ESP32-Media-Player/ESP32-Media-Player.ino
@@ -12,3 +12,16 @@ const unsigned long debounce_ms = 10; //button clicking speed
 const unsigned long holdpress_ms = 300; //holding the button speed
 const unsigned long repeat_ms = 150;//for volume step speed
 
+struct Button { //info that button needs: pin, if it is pressed, or hold, when is it press and released 
+  int pin; //what pin is it located
+  int Stabled; // debounced state High = idle and Low = Pressed
+  int read; //to tell what is going on
+  unsigned long LastChanged; //when changed happend
+  unsigned long pressedAt; //time when the pressing began
+  unsigned long LastRepeated;//for the volume feature 
+  bool isPressed, isActive; //boolean to test what is going on
+
+  Button(int gpio)
+  : pin(gpio),Stable(HIGH),read(LOW),LastChanged(0),pressedAt(0),LastRepeated(0),isPressed(false),isActive(false){} //default setting
+  Button() : Button(-1){}//Button with no arguments 
+}
