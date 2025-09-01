2d3764d5a3609a1ef77f75f001d78d83ede38b7a
KennethLe9 <kennethle9@gmail.com>
2025-09-01 14:22:55 -0500

    added global objects, setup and helpers functions

--- firmware/ESP32-Media-Player/ESP32-Media-Player.ino
+++ firmware/ESP32-Media-Player/ESP32-Media-Player.ino
@@ -4,9 +4,12 @@
 
 #include <BleKeyboard.h> //Using the ESP32 BLE Keyboard library 
 
+BleKeyboard bleKeyboard;
+
 const int Previous_Track = 18;
 const int Play_Pause = 19;
 const int Next_Track = 21;
+const int LED_Light = 2;// using the built in esp32 light 
 
 const unsigned long debounce_ms = 10; //button clicking speed
 const unsigned long holdpress_ms = 300; //holding the button speed
@@ -24,4 +27,19 @@ struct Button { //info that button needs: pin, if it is pressed, or hold, when i
   Button(int gpio)
   : pin(gpio),Stable(HIGH),read(LOW),LastChanged(0),pressedAt(0),LastRepeated(0),isPressed(false),isActive(false){} //default setting
   Button() : Button(-1){}//Button with no arguments 
+}
+//Global
+Button Prev(Previous_Track);
+Button Play(Play_Pause);
+Button Next(Next_Track);
+
+void setupPin(Button& b){ pinMode(b.pin, INPUT_PULLUP);}
+void led_blink(unsigned ms =40){ digitalWrite(LED_PIN, HIGH); delay(ms); digitalWrite(LED_PIN,LOW);}
+
+void setup(){
+  Serial.begin(115200);
+  setupPin(Prev); setupPin(Play); setupPin(Next);
+  pinMode(LED_PIN,OUTPUT);
+  digitalWrite(LED_PIN,LOW);
+  bleKeyboard.begin(); //output BT signal
 }
